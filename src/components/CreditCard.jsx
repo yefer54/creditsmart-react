6c9ded7835f14f556313da51e661b7b590d82162
yefer54 <villotapantojay@gmail.com>
2025-11-30 16:25:20 -0500

    Detalles graficos sobre la información de cada credito

--- src/components/CreditCard.jsx
+++ src/components/CreditCard.jsx
@@ -1,7 +1,49 @@
 import React from 'react'
 
-export const CreditCard = () => {
+export const CreditCard = ({ credit }) => {
+
+  const {
+    name,
+    description,
+    minAmount,
+    maxAmount,
+    interestRate,
+    maxTern,
+    requeriments,
+    icon,
+  } = credit;
+
   return (
-    <div>CreditCard</div>
+    <div className='credit-card'>
+
+      <div className='card-header'>
+        <span className='icon'>{icon}</span>
+        <h4>{name}</h4>
+      </div>
+
+      <p className='description'>{description}</p>
+
+      <div className='details'>
+        <div className='detail-item'>
+          <span className='label'>Tasa de intereses</span>
+          <span className='value'>{interestRate} % mensual</span>
+        </div>
+
+        <div className='detail-item'>
+          <span className='label'>Monto</span>
+          <span className='value'>
+            {formatCurrency(minAmount) - formatCurrency(maxAmount)}
+            </span>
+        </div>
+
+        <div className='detail-item'>
+          <span className='label'>Plazo</span>
+          <span className='value'>Hasta {maxTern} meses</span>
+        </div>
+      </div>
+
+      <button className='btn-primary'>Solicitar ahora </button>
+    </div>
   )
 }
+
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,3 +5,14 @@ export const Footer = () => {
     <div>Footer</div>
   )
 }
+
+<div class="card" style="width: 18rem;">
+      <img src= " ... " class="card-img-top" alt=" ... ">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">some quick example text</p>
+        <a href='#' class="btn btn-primary">Go somewhere</a>
+      </div>
+      </img>
+    </div>
+    

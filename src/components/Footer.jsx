a9e2a3fe31d9fb52c87f754289787f65a3d58001
yefer54 <villotapantojay@gmail.com>
2025-11-30 16:44:52 -0500

    Creación del footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
 
 export const Footer = () => {
+
   return (
-    <div>Footer</div>
+    <footer className='footer'>
+      <div className='container'>
+        <p>&copy; 2025 CreditSmart - IUDIGITAL </p>
+        <p>Todos los Derechos reservados</p>
+      </div>
+
+
+    </footer>
   )
 }
-
-<div class="card" style="width: 18rem;">
-      <img src= " ... " class="card-img-top" alt=" ... ">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">some quick example text</p>
-        <a href='#' class="btn btn-primary">Go somewhere</a>
-      </div>
-      </img>
-    </div>
-    

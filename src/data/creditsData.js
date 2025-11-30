3f4f3b4d55bcc5f709e876eafeac55bededd5128
yefer54 <villotapantojay@gmail.com>
2025-11-30 14:45:13 -0500

    Informacion respectiva sobre los creditos disponibles

---
+++ src/data/creditsData.js
@@ -0,0 +1,73 @@
+export const creditsData = [
+    {
+        id: 1,
+        name: "Credito de libre inversión",
+        description: "Dinero más rapido",
+        minAmount: 1000000,
+        maxAmount: 30000000,
+        interestRate: 1.8,
+        maxTern: 60,
+        requeriments: "Mayor de 18 años, ingresos minimos de $1.200.000",
+        icon: ""
+    },
+
+    {
+        id: 2,
+        name: "Credito de vehiculo",
+        description: "Consigue el vehiculo de tus sueños",
+        minAmount: 1000000,
+        maxAmount: 90000000,
+        interestRate: 2.1,
+        maxTern: 120,
+        requeriments: "Mayor de 18 años, ingresos minimos de $1.200.000",
+        icon: ""
+    },
+
+    {
+        id: 3,
+        name: "Credito de vivienda",
+        description: "Te prestamos dinero para que puedas formar un hogar",
+        minAmount: 1000000,
+        maxAmount: 200000000,
+        interestRate: 1.7,
+        maxTern: 170,
+        requeriments: "Mayor de 18 años, ingresos minimos de $1.200.000",
+        icon: ""
+    },
+
+    {
+        id: 4,
+        name: "Credito educativo",
+        description: "Aprovecha la oportunidad y concluye tu proceso educativo",
+        minAmount: 500000,
+        maxAmount: 10000000,
+        interestRate: 1.4,
+        maxTern: 60,
+        requeriments: "Mayor de 18 años, ingresos minimos de $1.200.000",
+        icon: ""
+    },
+
+    {
+        id: 5,
+        name: "Credito empresarial",
+        description: "Aqui te ayudamos a crear tu propio emprendimiento",
+        minAmount: 1000000,
+        maxAmount: 30000000,
+        interestRate: 1.8,
+        maxTern: 60,
+        requeriments: "Mayor de 18 años, ingresos minimos de $1.200.000",
+        icon: ""
+    },
+
+    {
+        id: 6,
+        name: "Credito de consumo",
+        description: "Dinero de poco monto, rapido y facil",
+        minAmount: 500000,
+        maxAmount: 10000000,
+        interestRate: 1.9,
+        maxTern: 80,
+        requeriments: "Mayor de 18 años, ingresos minimos de $1.200.000",
+        icon: ""
+    }
+]

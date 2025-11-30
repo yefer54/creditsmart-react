1bbf7e6c8e3d6bafb21acf122c5dbe2862b14dad
yefer54 <villotapantojay@gmail.com>
2025-11-30 18:20:12 -0500

    Diseño de la pagina de inicio de la aplicación

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { Hero } from'../components/Hero';
+import { CreditCard } from'../components/CreditCard';
+import { creditsData } from'../data/creditsData';
 
-const Home = () => {
+export const Home = () => {
+
+  console.log('Datos de creditos', creditsData);
+
   return (
-    <div>Home</div>
+    <>
+
+      <Hero
+      title= "Encuentra el credito perfecto para ti"
+      subtitle= "Tasas competitivas, aprobación rapida y sin tramites complicados"
+      />
+
+      <main className='container'>
+        <section className='credits-section'>
+          <h3>  Nuestros productos</h3>
+          <div className='credits-grid'>
+            {creditsData.map((credit) => {
+
+              return (
+                <CreditCard
+                  key={credit.id}
+                  credit={credit}
+                />
+              );
+            })}
+          </div>
+        </section>
+      </main>
+    </>
   )
 }
-
-export default Home
